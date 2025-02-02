b11983565d5b155ab463d641ea1a6db05d82c2db
Divyansh Dubey <93569892+idivyanshdubey@users.noreply.github.com>
2025-02-02 15:26:52 +0530

    Day 22

--- frontend/src/app/ipl/ipl.module.ts
+++ frontend/src/app/ipl/ipl.module.ts
@@ -1,38 +1,34 @@
-
-import { IplRoutingModule } from "./ipl-routing.module";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { HttpClientModule } from "@angular/common/http";
-import { TicketBookingComponent } from "./components/ticketbooking/ticketbooking.component";
-import { VoteComponent } from "./components/vote/vote.component";
-import { CricketerArrayComponent } from "./components/cricketerarray/cricketerarray.component";
-import { CricketerCreateComponent } from "./components/cricketercreate/cricketercreate.component";
-import { MatchCreateComponent } from "./components/matchcreate/matchcreate.component";
-import { TeamCreateComponent } from "./components/teamcreate/teamcreate.component";
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
+import { IplRoutingModule } from "./ipl-routing.module";
+import { FormGroup, FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { TeamCreateComponent } from "./components/teamcreate/teamcreate.component";
+import { CricketerCreateComponent } from "./components/cricketercreate/cricketercreate.component";
+import { MatchCreateComponent } from "./components/matchcreate/matchcreate.component";
+import { Match } from "./types/Match";
+import { RouterModule } from "@angular/router";
+import { TicketBookingComponent } from "./components/ticketbooking/ticketbooking.component";
+import { VoteComponent } from "./components/vote/vote.component";
+
 @NgModule({
   declarations: [
-    TicketBookingComponent,
-    VoteComponent,
-    CricketerArrayComponent,
+    TeamCreateComponent,
     CricketerCreateComponent,
     MatchCreateComponent,
-    TeamCreateComponent
+    TicketBookingComponent,
+    VoteComponent,
   ],
-
   imports: [
     CommonModule,
     ReactiveFormsModule,
     HttpClientModule,
-FormsModule
+    IplRoutingModule,
+    RouterModule
   ],
   exports: [
-    TeamCreateComponent,
-    MatchCreateComponent,
-    CricketerArrayComponent,
-    CricketerCreateComponent
-   
+    
   ]
 })
 export class IplModule {}
