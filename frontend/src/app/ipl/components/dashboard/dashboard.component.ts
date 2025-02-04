d96f9b802ea02725accac5e3c77a4ac8e65d3b00
Divyansh Dubey <93569892+idivyanshdubey@users.noreply.github.com>
2025-02-05 00:18:41 +0530

    Day 23

--- frontend/src/app/ipl/components/dashboard/dashboard.component.ts
+++ frontend/src/app/ipl/components/dashboard/dashboard.component.ts
@@ -1,5 +1,49 @@
 
+import { Component, OnInit } from "@angular/core";
+import { Team } from "../../types/Team";
+import { Cricketer } from "../../types/Cricketer";
+import { Match } from "../../types/Match";
+import { IplService } from "../../services/ipl.service";
+
+@Component({
+    selector:'app-dashboard',
+    templateUrl:'./dashboard.component.html',
+    styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent implements OnInit {
+  teams: Team[] = [];
+    cricketers: Cricketer[] = [];
+    matches: Match[] = [];
+
+    constructor(private iplService: IplService){}
+
+    ngOnInit(): void{
+        this.loadTeams();
+        this.loadCricketers();
+        this.loadMatches();
+    }
+
+    loadTeams(): void{
+        this.iplService.getAllTeams().subscribe((teams) => {
+            this.teams = teams;
+        });
+    }
+
+    loadCricketers(): void{
+        this.iplService.getAllCricketers().subscribe((cricketers) => {
+            this.cricketers = cricketers;
+        });
+    }
+
+    loadMatches(): void{
+        this.iplService.getAllMatches().subscribe((matches) => {
+            this.matches = matches;
+        });
+    }
+
+
 export class DashboardComponent  {
  
+
 
 }
